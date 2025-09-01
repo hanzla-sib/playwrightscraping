8089683ba0d7dc6737c843f8ba405f17853ec202
Hanzla Sibghat <hanzlasib@gmail.com>
2025-09-01 11:51:43 +0200

    feat: update request handler to log collection titles from Shopify

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,16 @@
-// For more information, see https://crawlee.dev/
-import { PlaywrightCrawler } from 'crawlee';
+import { PlaywrightCrawler } from "crawlee";
 
-// PlaywrightCrawler crawls the web using a headless
-// browser controlled by the Playwright library.
 const crawler = new PlaywrightCrawler({
-    // Use the requestHandler to process each of the crawled pages.
-    async requestHandler({ request, page, enqueueLinks, log, pushData }) {
-        const title = await page.title();
-        log.info(`Title of ${request.loadedUrl} is '${title}'`);
+  requestHandler: async ({ page }) => {
+    await page.waitForSelector(".collection-block-item");
+    const title = await page.$$eval(".collection-block-item", (els) => els.map((el) => el.textContent));
+    console.log("Collection Titles:");
 
-        // Save results as JSON to ./storage/datasets/default
-        await pushData({ title, url: request.loadedUrl });
-
-        // Extract links from the current page
-        // and add them to the crawling queue.
-        await enqueueLinks();
-    },
-    // Comment this option to scrape the full website.
-    maxRequestsPerCrawl: 20,
-    // Uncomment this option to see the browser window.
-    // headless: false,
+    title.forEach((text, idx) => {
+        console.log(`${idx + 1}. ${text}`);
+    });
+  },
 });
 
-// Add first URL to the queue and start the crawl.
-await crawler.run(['https://crawlee.dev']);
+
+await crawler.run(["https://warehouse-theme-metal.myshopify.com/collections"]);
