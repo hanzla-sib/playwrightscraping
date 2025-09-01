31de0d189260cd13b77034e0f72d35bfb2a38535
Hanzla Sibghat <hanzlasib@gmail.com>
2025-09-01 12:17:36 +0200

    feat: enhance request handler to process product details and collection links

--- src/main.ts
+++ src/main.ts
@@ -13,13 +13,47 @@ import { PlaywrightCrawler } from "crawlee";
 // });
 
 const crawler = new PlaywrightCrawler({
-  requestHandler: async ({ page, enqueueLinks }) => {
-    const collectionSelectors = ".collection-block-item";
-    await page.waitForSelector(collectionSelectors);
-    await enqueueLinks({
-      selector: collectionSelectors,
-      label: "COLLECTION",
-    });
+  requestHandler: async ({ page, enqueueLinks, request }) => {
+    // console.log(`Processing: ${request.label}`);
+
+    if (request.label === "DETAIL") {
+      const title = await page.locator(".product-meta h1").textContent();
+      const sku = await page.locator(".product-meta__sku-number").textContent();
+      console.log("title:", title);
+      console.log("sku:", sku);
+
+      const priceElement = page
+        .locator("span.price")
+        .filter({ hasText: "$" })
+        .first();
+      const currentPrice = await priceElement.textContent();
+      const rawprice = currentPrice?.split("$")[1]?.trim();
+      const price = Number(rawprice?.replace(",", ""));
+      console.log("price:", price);
+    } else if (request.label === "COLLECTION") {
+      const productSelectors = ".product-item > a";
+      const nextPageSelector = "a.pagination__next";
+      await page.waitForSelector(productSelectors);
+      await enqueueLinks({
+        selector: productSelectors,
+        label: "DETAIL",
+      });
+
+      const nextButton = await page.$(nextPageSelector);
+      if (nextButton) {
+        await enqueueLinks({
+          selector: nextPageSelector,
+          label: "COLLECTION",
+        });
+      }
+    } else {
+      const collectionSelectors = ".collection-block-item";
+      await page.waitForSelector(collectionSelectors);
+      await enqueueLinks({
+        selector: collectionSelectors,
+        label: "COLLECTION",
+      });
+    }
   },
 });
 
