60996ed8d8a6b3f6c93cbec295065749762f4f0a
Hanzla Sibghat <hanzlasib@gmail.com>
2025-09-01 11:54:06 +0200

    feat: refactor request handler to enqueue collection links instead of logging titles

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import { PlaywrightCrawler } from "crawlee";
 
-const crawler = new PlaywrightCrawler({
-  requestHandler: async ({ page }) => {
-    await page.waitForSelector(".collection-block-item");
-    const title = await page.$$eval(".collection-block-item", (els) => els.map((el) => el.textContent));
-    console.log("Collection Titles:");
+// const crawler = new PlaywrightCrawler({
+//   requestHandler: async ({ page }) => {
+//     await page.waitForSelector(".collection-block-item");
+//     const title = await page.$$eval(".collection-block-item", (els) => els.map((el) => el.textContent));
+//     console.log("Collection Titles:");
 
-    title.forEach((text, idx) => {
-        console.log(`${idx + 1}. ${text}`);
+//     title.forEach((text, idx) => {
+//         console.log(`${idx + 1}. ${text}`);
+//     });
+//   },
+// });
+
+const crawler = new PlaywrightCrawler({
+  requestHandler: async ({ page, enqueueLinks }) => {
+    const collectionSelectors = ".collection-block-item";
+    await page.waitForSelector(collectionSelectors);
+    await enqueueLinks({
+      selector: collectionSelectors,
+      label: "COLLECTION",
     });
   },
 });
-
 
 await crawler.run(["https://warehouse-theme-metal.myshopify.com/collections"]);
